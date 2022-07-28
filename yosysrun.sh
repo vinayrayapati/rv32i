472f09930f7f6f5a4992c87c0d2d2a46f767e8be
vinayrayapati <110079631+vinayrayapati@users.noreply.github.com>
2022-07-28 10:26:08 +0530

    Add files via upload

---
+++ yosysrun.sh
@@ -0,0 +1,14 @@
+# read design
+
+read_verilog iiitb_rv32i.v
+
+# generic synthesis
+synth -top iiitb_rv32i
+
+# mapping to mycells.lib
+dfflibmap -liberty ‌‌/home/vinay.r/Desktop/iiitb_rv32i/lib/‌sky130_fd_sc_hd__tt_025C_1v80.lib
+abc -liberty /home/vinay.r/Desktop/iiitb_rv32i/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
+clean
+flatten
+# write synthesized design
+write_verilog -assert .v
