fec5927d6613e5ceafeb5c42b53e8ad1725cf977
vinayrayapati <110079631+vinayrayapati@users.noreply.github.com>
2022-08-04 17:43:37 +0530

    Update iiitb_rv32i_tb.v

--- iiitb_rv32i_tb.v
+++ iiitb_rv32i_tb.v
@@ -2,7 +2,6 @@ module iiitb_rv32i_tb;
 
 reg clk;
 integer k;
-
 wire [31:0]WB_OUT,PC;
 
 iiitb_rv32i rv32(clk,WB_OUT,PC);
@@ -13,36 +12,34 @@ always #10 clk=!clk;
 
 initial begin 
 
-  $dumpfile ("iiitb_rv32i.vcd"); //by default vcd
-  $dumpvars (0, iiitb_rv32i_tb);
+$dumpfile ("rv32i.vcd"); //by default vcd
+$dumpvars (0, iiitb_rv32i_tb);
 
 for(k=0;k<32;k=k+1) 
 rv32.REG[k] <=k;
 
-//rv32.Reg[0] =0;
-//rv32.Reg[1] =1;
-//rv32.Reg[2] =2;
-//rv32.Reg[3] =3;
-//rv32.reg[4]=4;
-
 rv32.PC = 0;
 
 
-rv32.MEM[0] = 32'h00222000;        // add r4,r0,r1.(i1)
-rv32.MEM[1] = 32'h04432800;         //SUB R5,R2,R3.(i2)
-rv32.MEM[2] = 32'h10E60001;         //ADI R6,R7,IMM.(i3)
+rv32.MEM[0] = 32'h00208300;         // add r6,r1,r2.(i1)
+rv32.MEM[1] = 32'h00209380;         //sub r7,r1,r2.(i2)
+rv32.MEM[2] = 32'h0030a400;         //and r8,r1,r3.(i3)
+rv32.MEM[3] = 32'h00513480;         //or r9,r2,r5.(i4)
+rv32.MEM[4] = 32'h0040c500;         //xor r10,r1,r4.(i5)
+rv32.MEM[5] = 32'h00415580;         //slt r11,r2,r4.(i6)
+rv32.MEM[6] = 32'h00520601;         //addi r12,r4,5.(i7)
+rv32.MEM[7] = 32'h00209184;         //sw r3,r1,2.(i8)
+rv32.MEM[8] = 32'h00208684;         //lw r13,r1,2.(i9)
+rv32.MEM[9] = 32'h00f00002;         //beq r0,r0,15.(i10)
+rv32.MEM[26] = 32'h00210700;         //add r14,r2,r2.(i11)
+rv32.MEM[27] = 32'h01409002;         //bne r0,r1,20.(i12)
+rv32.MEM[49] = 32'h00520601;         //addi r12,r4,5.(i13)
+rv32.MEM[50] = 32'h00208783;         //sll r15,r1,r2(2).(i14)
+rv32.MEM[51] = 32'h00271803;         //srl r16,r14,r2(2).(i15)
 
 
-rv32.MEM[3] = 32'h1D090001;  //st r8 r9 imm=1.(i4)
 
-rv32.MEM[4]=32'h190A0001; //lt r10 r8 imm=1.(i5)
-
-rv32.MEM[5]=32'h20000028;//(i6)
-
-
-rv32.MEM[47]=32'h01AE6000;//(i7)//add r12 r13,r14
-
-#500 $finish;
+#2000 $finish;
 
 
 end
